9e703818175eaa9878213e8718f0df054039a497
Raúl <raulbaeza8>
2023-11-23 12:46:30 +0100

    inicio de caja blanca

--- Caja Blanca/BST/src/main/java/Test_insert.java
+++ Caja Blanca/BST/src/main/java/Test_insert.java
@@ -1,2 +1,22 @@
+import com.bst.BST;
+import com.bst.Node;
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.io.FileReader;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.*;
 public class Test_insert {
+    @org.junit.jupiter.params.ParameterizedTest
+    @org.junit.jupiter.params.provider.CsvFileSource(resources = "TestCB - Insert.csv",numLinesToSkip = 1)
+    void testCSV(String ruta, String root, boolean recursive){
+        BST arbol = new BST();
+        arbol.depth();
+        if (false){
+            System.out.println("hola");
+        }
+
+
+    }
 }
