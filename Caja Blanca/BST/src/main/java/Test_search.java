1845d77b74ca17d052c4f0661e4ef498a6aeca58
Raúl <raulbaeza8>
2023-12-13 11:36:48 +0100

    Empiezo search

---
+++ Caja Blanca/BST/src/main/java/Test_search.java
@@ -0,0 +1,12 @@
+
+import com.bst.BST;
+import com.bst.Node;
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.io.FileReader;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.*;
+public class Test_search {
+}
